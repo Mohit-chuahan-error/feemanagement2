9339245a5315f66c3c846db9c8e0e913e382fa52
mohit <mohit@LAPTOP-F9ISQF3B>
2023-06-18 12:19:54 +0530

    commit

--- FeeManagementBootstrap/src/com/javatpoint/dao/DB.java
+++ FeeManagementBootstrap/src/com/javatpoint/dao/DB.java
@@ -7,7 +7,7 @@ public static Connection getCon(){
 		Class.forName("com.mysql.jdbc.Driver");
 		
 		con=DriverManager.getConnection("jdbc:mysql://localhost:3306/fee","root","Errormysql");
-	}catch(Exception ex){System.out.println(ex+"Eror");}
+	}catch(Exception ex){System.out.println(ex+"Erorr");}
 	return con;
 }
 }
